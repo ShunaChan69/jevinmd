57a439e0c4c3f2cde385a73b9372a7138ac9815d
ShunaChan69 <111638506+ShunaChan69@users.noreply.github.com>
2022-08-25 20:36:15 +0700

    Update pctlove.js

--- plugins/pctlove.js
+++ plugins/pctlove.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .pictlove jevin'
+  if (!args[0]) throw 'Masukkan Text\nContoh : .pictlove kanna'
   m.reply('_Proses..._')
   let res = `https://botcahx-rest-api.herokuapp.com/api/photooxy/picture-of-love?text=${response[0]}`
-  conn.sendFile(m.chat, res, 'botcahx.jpg', `© Jevin`, m, false)
+  conn.sendFile(m.chat, res, 'botcahx.jpg', `© KannaChan`, m, false)
 }
 handler.help = ['pictlove'].map(v => v + ' <text>')
 handler.tags = ['maker']
