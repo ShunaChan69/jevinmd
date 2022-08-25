09c0d4ac89052e4d5d3443b9e365e4a87bba738b
ShunaChan69 <111638506+ShunaChan69@users.noreply.github.com>
2022-08-25 19:37:05 +0700

    Update _restmaker2.js

--- plugins/_restmaker2.js
+++ plugins/_restmaker2.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .flaming4 jevin'
+  if (!args[0]) throw 'Masukkan Text\nContoh : .flaming8 kanna'
   m.reply('_Proses..._')
   let res = `https://botcahx-rest-api.herokuapp.com/api/photooxy/shadow-sky?text=${response[0]}`
-  conn.sendFile(m.chat, res, 'botcahx.jpg', `© Jevin`, m, false)
+  conn.sendFile(m.chat, res, 'botcahx.jpg', `© KannaChan`, m, false)
 }
 handler.help = ['flaming8'].map(v => v + ' <text>')
 handler.tags = ['maker']
