ec07235b438ff1a97e833276d4ea0a5f96e1a285
ShunaChan69 <111638506+ShunaChan69@users.noreply.github.com>
2022-08-25 20:23:41 +0700

    Update main-belibot.js

--- plugins/main-belibot.js
+++ plugins/main-belibot.js
@@ -3,7 +3,7 @@ let info = `
 • Berikut list harga sewa bot
 • Bingung? silahkan chat owner
 
-https://wa.me/6281210283436 (Jevin)
+https://wa.me/19047502362 (Kanna Chan)
 `
 const sections = [
    {
@@ -16,22 +16,13 @@ const sections = [
 	{title: "VIP", rowId: '.vip1 *Paket:* VIP • Sewa', description: 'Price: 100k (12 bulan)' },
 	]
     }, {
-    title: `250 FITUR`,
+    title: `PREMIUM USER`,
 	rows: [
 	{title: "HEMAT", rowId: '.hemat2 *Paket:* HEMAT • Premium', description: 'Price: 5k (1 minggu)' },
 	{title: "NORMAL", rowId: '.normal2 *Paket:* NORMAL • Premium', description: 'Price: 10k (1 bulan)' },
 	{title: "PRO", rowId: '.pro2 *Paket:* PRO • Premium', description: 'Price: 30k (4 bulan)' },
 	{title: "VIP", rowId: '.vip2 *Paket:* VIP • Premium', description: 'Price: 50k (8 bulan)' },
 	{title: "VVIP", rowId: '.vvip *Paket:* PERMANENT • Premium', description: 'Price: 70k (12 bulan)' },
-	]
-    }, {
-	title: `150 FITUR`,
-	rows: [
-	{title: "HEMAT", rowId: '.hemat3 *Paket:* HEMAT • Sewa', description: 'Price: 5k (1 bulan)' },
-	{title: "NORMAL", rowId: '.normal3 *Paket:* NORMAL • Sewa', description: 'Price: 10k (70 hari)' },
-	{title: "STANDAR", rowId: '.standar3 *Paket:* STANDAR • Sewa', description: 'Price: 20k (4 bulan)' },
-	{title: "PRO", rowId: '.pro3 *Paket:* PRO • Sewa', description: 'Price: 40k (8 bulan)' },
-	{title: "VIP", rowId: '.vip3 *Paket:* VIP • Sewa', description: 'Price: 50k (12 bulan)' },
 	]
     }, 
 ]
