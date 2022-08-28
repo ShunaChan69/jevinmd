a9d322d4a8171f00153f6149acb792493778ba25
ShunaChan69 <111638506+ShunaChan69@users.noreply.github.com>
2022-08-28 09:16:42 +0700

    Update maker-butterfly.js

--- plugins/maker-butterfly.js
+++ plugins/maker-butterfly.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : . butterfly BOTCAHX'
+  if (!args[0]) throw 'Masukkan Text\nContoh : . butterfly ORDISTON'
   m.reply('_Proses..._')
   let res = `https://botcahx-rest-api.herokuapp.com/api/photooxy/butterfly?text=${response[0]}`
-  conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
+  conn.sendFile(m.chat, res, 'botcahx.jpg', `© ORDISTON`, m, false)
 }
 handler.help = ['butterfly'].map(v => v + ' <text>')
 handler.tags = ['maker']
